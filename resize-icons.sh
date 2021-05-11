bdd610f5fb64e90f0b15800c64daabb624a76316
Ash Kyd <ash@kyd.com.au>
2021-05-11 20:58:44 +1000

    tidy up icons

--- resize-icons.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-convert icon.png -resize 72x72 images/icons/icon-72x72.png
-convert icon.png -resize 96x96 images/icons/icon-96x96.png
-convert icon.png -resize 128x128 images/icons/icon-128x128.png
-convert icon.png -resize 144x144 images/icons/icon-144x144.png
-convert icon.png -resize 152x152 images/icons/icon-152x152.png
-convert icon.png -resize 192x192 images/icons/icon-192x192.png
-convert icon.png -resize 384x384 images/icons/icon-384x384.png
-convert icon.png -resize 512x512 images/icons/icon-512x512.png
